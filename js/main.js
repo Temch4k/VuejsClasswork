d0aa862c05311e3b944a2315536d951a3f134e31
Temch4k <artsiom.skarakhod@gmail.com>
2021-03-08 20:12:01 -0700

    worked on classwork

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            keyword: '',
+            result: null
+        }
+    },
+    methods: {
+        searchGoogleBooks() {
+            fetch('https://www.googleapis.com/books/v1/volumes?&q=' + this.keyword + "&startIndex=0&maxResults=27")
+                .then(response => response.json())
+                .then(json => this.result = json)
+        }
+    }
+
+})
